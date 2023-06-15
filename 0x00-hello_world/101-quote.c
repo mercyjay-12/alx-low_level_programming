e5d371346ef172803a4f7bcaf00153813b18aa09
mercyjay-12 <mercyjaluo@gmail.com>
2023-06-15 12:43:45 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
 /**
 * main - Entry point
 *
@@ -7,6 +6,6 @@
 */
 int main(void)
 {
-write(2,"and that piece of art is usefl\" - Dora Korpar, 2015-10-19\n", 59);
+system("echo 'and that piece of art is usefl\" \ - Dora Korpar, 2015-10-19'");
 return (1);
 }
